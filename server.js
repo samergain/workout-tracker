e9fc1245b80c4dac170b97df19019901c6d1b9bf
samergain <samersadon@gmail.com>
2020-11-11 01:30:25 -0500

    workouts routes with GET PUSH PUT are working and GET exercise

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
 const Workout = require("./models/workout-model");
-
+const db = require("./models");
 const app = express();
 
 app.use(logger("dev"));
@@ -18,6 +18,60 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB", { useNewUrlParser: true });
 
 //////routes/////////
+var path = require("path");
+//   GET  /api/workouts
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+      .then(result => {
+        res.send(result);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+//   POST /api/workouts
+app.post("/api/workouts", ({body}, res) => {
+        const workout = new Workout(body);
+        Workout.create(workout)
+          .then(result => {
+            res.json(result);
+          })
+          .catch(err => {
+            res.json(err);
+          });
+});
+
+//   PUT  /api/workouts/:id
+app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.update(
+      {
+        _id: mongoose.Types.ObjectId(req.params.id)
+      },
+      {
+        $set: {
+            exercises: req.body
+        }
+      },
+      (error, edited) => {
+        if (error) {
+          console.log(error);
+          res.send(error);
+        } else {
+          console.log(edited);
+          res.send(edited);
+        }
+      }
+    );
+  });
+//   GET  /api/workouts/range
+//   GET  /exercise
+app.get("/exercise", (req,res) => {
+    res.sendFile(path.join(__dirname, "/public/exercise.html"));
+})
+//   GET  /stats
+app.get("/stats", (req,res) => {
+    res.sendFile(path.join(__dirname, "/public/stats.html"));
+})
 
 
 app.listen(PORT, () => {
