63053ceb7b644d9e5b8496e8f88ba1cbf4dc69d4
samergain <samersadon@gmail.com>
2020-11-10 22:50:45 -0500

    main files structure with server connection

---
+++ models/workout-model.js
@@ -0,0 +1,15 @@
+// let workoutSeed = [
+//     {
+//       day: new Date().setDate(new Date().getDate()-10),
+//       exercises: [
+//         {
+//           type: "resistance",
+//           name: "Bicep Curl",
+//           duration: 20,
+//           weight: 100,
+//           reps: 10,
+//           sets: 4,
+//           distance: 4
+//         }
+//       ]
+//     }];
