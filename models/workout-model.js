2078f22f5efc9c7a443cfba86cb57f2e674e2d03
samergain <samersadon@gmail.com>
2020-11-10 23:08:35 -0500

    workout model created

--- models/workout-model.js
+++ models/workout-model.js
@@ -1,15 +1,32 @@
-// let workoutSeed = [
-//     {
-//       day: new Date().setDate(new Date().getDate()-10),
-//       exercises: [
-//         {
-//           type: "resistance",
-//           name: "Bicep Curl",
-//           duration: 20,
-//           weight: 100,
-//           reps: 10,
-//           sets: 4,
-//           distance: 4
-//         }
-//       ]
-//     }];
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: 
+    [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Type is Required"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Name is Required"
+            },
+            duration: {type: Number},
+            weight: {type: Number},
+            reps: {type: Number},
+            sets: {type: Number},
+            distance: {type: Number}
+        }
+    ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+module.exports = Workout;
+
